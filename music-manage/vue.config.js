9c012ba8713e17cfad9d98d8328b64fa8ee57717
zty-f <2559247260@qq.com>
2022-07-02 19:11:08 +0800

    前端初始化部署，后续根据后端情况进行修改！

---
+++ music-manage/vue.config.js
@@ -0,0 +1,18 @@
+const {defineConfig} = require('@vue/cli-service')
+
+module.exports = defineConfig({
+    transpileDependencies: true,
+    chainWebpack: config => {
+        config.plugin('define').tap(definitions => {
+            Object.assign(definitions[0]['process.env'], {
+                NODE_HOST: '"http://localhost:8888"',
+            });
+            return definitions;
+        });
+    },
+    devServer: {
+        port: 8081,  // 端口号的配置
+        host:'localhost',
+        open: true   // 自动打开浏览器
+    }
+})
